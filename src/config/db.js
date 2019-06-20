bfe719c4b012a4973511d1f9a2cfe6771542c43a
cuixin <wb-cx504210@alibaba-inc.com>
2019-06-20 10:46:08 +0800

    add

--- src/config/db.js
+++ src/config/db.js
@@ -1,31 +1,15 @@
-const env = process.env.NODE_DEV //环境参数
-let MYSQL_CONFIG;
-let REDIS_CONFIG;
+// const env = process.env.NODE_DEV //环境参数
+let MYSQL_CONFIG = {
+  host:'localhost',
+  user:'root',
+  password:'',
+  port:'3306',
+  database:'myblog'
+}
 
-if(env === 'env'){
-  MYSQL_CONFIG = {
-    host:'localhost',
-    user:'root',
-    password:'',
-    port:'3306',
-    database:'myblog'
-  }
-  REDIS_CONFIG = {
-    port:6379,
-    host:'127.0.0.1'
-  }
-}else if(env === 'production'){
-  MYSQL_CONFIG = {
-    host:'localhost',
-    user:'root',
-    password:'',
-    port:'3306',
-    database:'myblog'
-  }
-  REDIS_CONFIG = {
-    port:6379,
-    host:'127.0.0.1'
-  }
+let REDIS_CONFIG = {
+  port:6379,
+  host:'127.0.0.1'
 }
 
 
--- src/routers/user.js
+++ src/routers/user.js
@@ -4,8 +4,8 @@ const { SuccessModel,ErrorModel } = require('../model/resModel')
 const handleUserRouter = (req,res) => {
     const method = req.method
     //登陆
-    if(method === 'POST' && req.path === '/api/user/login'){
-        const { username, password} = require.body
+    if(method === 'GET' && req.path === '/api/user/login'){
+        const { username, password} = req.query
         const result = login(username,password)
         return result.then(data=>{
             if(data.username){
